48d4f9c91fe731ff47859190e8c2ef6c855afd53
mauricio-bolanos <mau.bolanosr@gmail.com>
2025-03-30 14:21:34 -0600

    Inicialización del proyecto

---
+++ app/src/main/java/mauricio/u/latina/bolanos/rosales/MainActivity.kt
@@ -0,0 +1,47 @@
+package mauricio.u.latina.bolanos.rosales
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mauricio.u.latina.bolanos.rosales.ui.theme.ProyectoFinalTheme
+
+abstract class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProyectoFinalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProyectoFinalTheme {
+        Greeting("Android")
+    }
+}

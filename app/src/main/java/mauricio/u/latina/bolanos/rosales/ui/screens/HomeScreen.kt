aea2093b3290c818b3058cfaa4f8870ccb786b10
mauricio-bolanos <mau.bolanosr@gmail.com>
2025-04-24 13:41:10 -0600

    Correcciones para homescreen

--- app/src/main/java/mauricio/u/latina/bolanos/rosales/ui/screens/HomeScreen.kt
+++ app/src/main/java/mauricio/u/latina/bolanos/rosales/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package mauricio.u.latina.bolanos.rosales.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -99,9 +100,11 @@ fun HomeScreen(
             }
         }
 
-        // FAB para agregar nueva partida
         FloatingActionButton(
-            onClick = { navController.navigate("addPartida") },
+            onClick = {
+                Log.d("NAVIGATION", "Navigating to addPartida")
+                navController.navigate("addPartida")
+            },
             modifier = Modifier
                 .align(Alignment.BottomEnd)
                 .padding(16.dp)
